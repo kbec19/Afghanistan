f42186ec74cdf87771e58c4a5e5df5f38cfc55bc
kbec19 <kbecvar@umass.edu>
2023-08-15 15:41:59 -0500

    Commit Next Data

--- Afghanistan.R
+++ Afghanistan.R
@@ -11,24 +11,31 @@ library(Hmisc)
 
 #Load quantitative data sheets and check column names
 
-AfghanistanData_AllData_CleanedCoded_FINAL <- read_csv("~/Documents/GitHub/Afghanistan_Survey.csv")
-afghan_data <- AfghanistanData_AllData_CleanedCoded_FINAL
-afghan_10a <- read_csv("~/Documents/GitHub/AfghanistanData_10a.csv")
-afghan_10b <- read_csv("~/Documents/GitHub/AfghanistanData_10b.csv")
-afghan_10c <- read_csv("~/Documents/GitHub/AfghanistanData_10c.csv")
+AfghanistanData_Quant <- read_csv("~/Documents/GitHub/Afghanistan/Afghanistan_Survey.csv")
+afghan_data <- AfghanistanData_Quant
+afghan_7 <- read_csv("~/Documents/GitHub/Afghanistan/AfghanistanData_7.csv")
+afghan_10a <- read_csv("~/Documents/GitHub/Afghanistan/AfghanistanData_10a.csv")
+afghan_10b <- read_csv("~/Documents/GitHub/Afghanistan/AfghanistanData_10b.csv")
+afghan_10c <- read_csv("~/Documents/GitHub/Afghanistan/AfghanistanData_10c.csv")
+afghan_11a <- read_csv("~/Documents/GitHub/Afghanistan/AfghanistanData_11a.csv")
+afghan_17 <- read_csv("~/Documents/GitHub/Afghanistan/AfghanistanData_17.csv")
+
 names(afghan_data)
 
 #Create master list of all UUIDs who have answered one of the open-ends
 
-data_joined_a <- full_join(afghan_data, afghan_10a, by="UUID")
-data_joined_b <- full_join(data_joined_a, afghan_10b, by="UUID")
-data_joined_full <- full_join(data_joined_b, afghan_10c, by="UUID")
+data_joined_7 <- full_join(afghan_data, afghan_7, by="UUID")
+data_joined_10a <- full_join(data_joined_7, afghan_10a, by="UUID")
+data_joined_10b <- full_join(data_joined_10a, afghan_10b, by="UUID")
+data_joined_10c <- full_join(data_joined_10b, afghan_10c, by="UUID")
+data_joined_11a <- full_join(data_joined_10c, afghan_11a, by="UUID")
+data_joined_full <- full_join(data_joined_11a, afghan_17, by="UUID")
 
 #Frequency distribution of question 9, 
 #'How much would you agree or disagree with the following sentence: “I believe achieving human rights for women is among the top priorities for the future of my country”?'
 
-freq(afghan_data$q09_human_rights_women)
-freq(afghan_data$q10a_women_rights_priorities_open_end_translated)
+freq(data_joined_full$q09_human_rights_women)
+freq(data_joined_full$q22_religion_importance)
 
 #Analytical Setup
 
